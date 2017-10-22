fa190b389b612db9efafd0403d28baddb2da4e9d
Miiro Juuso <miiro.juuso@gmail.com>
2017-10-22 21:31:54 +0100

    chore: restructuring terraform config

--- lesson32-terraform/resources.tf
+++ lesson32-terraform/resources.tf
@@ -118,11 +118,13 @@ resource "aws_key_pair" "root" {
 resource "aws_instance" "gid-app" {
     ami = "ami-eed00d97"
     instance_type = "t2.micro"
+    count = 2
+
     tags {
-        Name = "node-app${count.index}"
+        Name = "gid-app${count.index}"
         Author = "Get into DevOps"
     }
-    count = 2
+
     subnet_id = "${element(aws_default_subnet.default.*.id, count.index)}"
     associate_public_ip_address = true
 
@@ -131,7 +133,8 @@ resource "aws_instance" "gid-app" {
 
     provisioner "remote-exec" {
         inline = [
-            "sudo apt update && sudo apt -y install nginx"
+            "sudo apt update",
+            "sudo apt -y install nginx"
         ]
         connection {
             user = "ubuntu"
@@ -148,18 +151,11 @@ resource "aws_lb" "gid" {
   name            = "gid-lb"
   internal        = false
   security_groups = ["${aws_security_group.gid-lb.id}"]
-  subnets         = ["${aws_default_subnet.default.0.id}", "${aws_default_subnet.default.1.id}"]
+  subnets         = ["${aws_default_subnet.default.*.id}"]
 
   tags {
     Author = "Get into DevOps"
   }
-}
-
-resource "aws_lb_target_group" "gid" {
-  name     = "gid-lb-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = "${aws_default_vpc.default.id}"
 }
 
 resource "aws_lb_listener" "gid" {
@@ -171,6 +167,13 @@ resource "aws_lb_listener" "gid" {
     target_group_arn = "${aws_lb_target_group.gid.arn}"
     type             = "forward"
   }
+}
+
+resource "aws_lb_target_group" "gid" {
+  name     = "gid-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${aws_default_vpc.default.id}"
 }
 
 resource "aws_lb_target_group_attachment" "test" {
